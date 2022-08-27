54000d41b4501a009445be7cd4c37f1cac53dcc5
Davi Vitorino <davitorino26@gmail.com>
2022-08-27 13:56:19 -0300

    feat: all code from todo list project

---
+++ src/stores/todoList.js
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+
+export const useTodoListStore = defineStore('todoList', {
+  state: () => ({
+    todoList: [],
+    id: 0
+  }),
+  actions: {
+    addTodo(item) {
+      this.todoList.push({ item, id: this.id++, completed: false })
+    },
+    deleteTodo(itemId) {
+      this.todoList = this.todoList.filter((todo) => {
+        return todo.id !== itemId;
+      })
+    },
+    toggleCompleted(idToFind) {
+      const todo = this.todoList.find((todo) => todo.id === idToFind);
+      if (todo) {
+        todo.completed = !todo.completed
+      }
+    },
+  }
+})
